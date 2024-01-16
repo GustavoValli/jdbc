ca63c092b13c3817f63960562b922687018f6b45
GustavoValli <valligustavo2005@gmail.com>
2024-01-16 19:08:07 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,13 +10,16 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+        /*Department obj = new Department(1, "Books");
         System.out.println(obj);
 
         Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
+        System.out.println(seller);*/
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller1 = sellerDao.findById(3);
+        System.out.println(seller1);
 
     }
 }
